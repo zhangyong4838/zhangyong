589f913d5999f9bcd986572b677e0ad51b4d7f74
yong.zhang <yong.zhang@zlrt.net>
2020-07-14 14:42:20 +0800

    zz

--- myapp/src/utils/create.js
+++ myapp/src/utils/create.js
@@ -3,24 +3,35 @@ import Vue from 'vue';
 
 export default function create(component,props){
     // 方案一：使用vue.exend()
+    const Ctor = Vue.extend(component)
+    // 创建组件实例
+    const comp = new Ctor({propsData:props})
 
-
-    // 方案二：借鸡生蛋思想
-    const vm = new Vue({
-        render(h){
-            // 组件配置对象
-            return h(component,{props})
-        }
-    }).$mount()
-    // 手动追加dom元素
-    document.body.appendChild(vm.$el)
-
-    const comp = vm.$children[0]
+    comp.$mount()
+    document.body.appendChild(comp.$el)
 
     // 清除dom
     comp.remove = () =>{
-        document.body.removeChild(vm.$el)
+        document.body.removeChild(comp.$el)
         comp.$destroy()
     }
+
+    // // 方案二：借鸡生蛋思想
+    // const vm = new Vue({
+    //     render(h){
+    //         // 组件配置对象
+    //         return h(component,{props})
+    //     }
+    // }).$mount()
+    // // 手动追加dom元素
+    // document.body.appendChild(vm.$el)
+
+    // const comp = vm.$children[0]
+
+    // // 清除dom
+    // comp.remove = () =>{
+    //     document.body.removeChild(vm.$el)
+    //     comp.$destroy()
+    // }
     return comp
 }
